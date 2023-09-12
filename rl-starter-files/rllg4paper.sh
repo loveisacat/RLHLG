5f19adfbf104d0860f3a22154d97579076ca168c
Bin Chen <bc3607@gadi-login-02.gadi.nci.org.au>
2023-09-12 18:46:09 +1000

    fix

--- rl-starter-files/rllg4paper.sh
+++ rl-starter-files/rllg4paper.sh
@@ -10,6 +10,8 @@
 #PBS -l wd
   
 module load python3/3.10.0
-python3 -m scripts.train --algo ppo --env MiniGrid-LavaCrossingS9N1-v0 --model crossing_task_A_0 --save-interval 100 --frames 2000000  --seed 9 > result/crossing_task_A
-python3 -m scripts.train --algo ppo --env MiniGrid-LavaCrossingS9N1-v0 --model crossing_task_A_1 --save-interval 100 --frames 2000000  --seed 10 > result/crossing_task_A
+python3 -m scripts.train --algo ppo --env MiniGrid-LavaCrossingS9N2-v0 --model crossing_task_B_0 --save-interval 100 --frames 5000000  --seed 9 > result/crossing_task_A
+python3 -m scripts.train --algo ppo --env MiniGrid-LavaCrossingS9N2-v0 --model crossing_task_B_1 --save-interval 100 --frames 5000000  --seed 10 > result/crossing_task_A
+python3 -m scripts.train --algo ppo --env MiniGrid-LavaCrossingS9N2-v0 --model crossing_task_B_2 --save-interval 100 --frames 5000000  --seed 11 > result/crossing_task_A
+python3 -m scripts.train --algo ppo --env MiniGrid-LavaCrossingS9N2-v0 --model crossing_task_B_3 --save-interval 100 --frames 5000000  --seed 12 > result/crossing_task_A
 
